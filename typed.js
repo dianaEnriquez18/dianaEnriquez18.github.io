d36b074fa166ef254f7f1882d7abdc73984a8364
dianaEnriquez18 <denriquezbx@gmail.com>
2024-07-24 11:03:57 -0600

    Update typed.js

--- typed.js
+++ typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed (".typing", {
-    strings: ["Diseñadora digital","Diseñadora UX/UI", "Ilustradora"],
+    strings: ["Diseñadora grafica","Diseñadora UX/UI", "Diseñadora digital"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
